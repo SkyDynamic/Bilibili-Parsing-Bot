93789b4e6d8d1ff64309ac3b87e560b1212fe29f
Sky_Dynamic <82658163+SkyDynamic@users.noreply.github.com>
2022-10-02 09:47:00 +0800

    去除了asyncio和nest_asyncio模块，不会再报event has loop了

--- src/plugins/bili/__init__.py
+++ src/plugins/bili/__init__.py
@@ -1,3 +1,4 @@
+from ast import Or
 from nonebot import on_command, on_keyword
 from nonebot.adapters.onebot.v11 import Bot, GROUP
 from nonebot.adapters.onebot.v11.event import GroupMessageEvent
@@ -7,6 +8,7 @@ from nonebot.params import CommandArg
 from .video import Video
 from .config import *
 
+import requests
 import re
 
 helpmessage = '''/video + BV号 解析视频
@@ -16,10 +18,10 @@ helpmessage = '''/video + BV号 解析视频
 当自动解析开启后，会自动解析信息中第一个BV号的视频
 '''
 
-video_msg = on_command('video', permission=GROUP, priority=50)
+video_msg = on_command('video', permission=GROUP, priority=0, block=True)
 @video_msg.handle()
 async def video_main(bot: Bot, mathcer: Matcher, event: GroupMessageEvent ,args: Message = CommandArg()):
-    info = Video(args)
+    info = await Video(args)
     data = info.data
     reply = MessageSegment.reply(event.message_id)
     if info.status == True:
@@ -31,11 +33,11 @@ async def video_main(bot: Bot, mathcer: Matcher, event: GroupMessageEvent ,args:
         desc = data.get('desc')
         author = data.get('owner').get('name')
         msg = f'视频号：{bvid}  av{avid}\n标题：{title}\n简介：\n{desc}\n\n类型：{tname}\n作者：{author}\n视频链接：\nhttps://bilibili.com/video/{bvid}\nhttps://bilibili.com/video/av{avid}'
-        await video_msg.send(reply + pic + msg)
+        await video_msg.finish(reply + pic + msg)
     if info.status == False:
-        await video_msg.send(reply + data)
+        await video_msg.finish(reply + data)
 
-auto_analysis = on_keyword(['BV','bv','bV','Bv'],permission=GROUP,priority=49)
+auto_analysis = on_keyword(['BV','bv','bV','Bv','b23.tv'],permission=GROUP,priority=49)
 @auto_analysis.handle()
 async def analysis(bot: Bot, mathcer: Matcher, event: GroupMessageEvent):
     group_json = Config_Read().group
@@ -45,10 +47,18 @@ async def analysis(bot: Bot, mathcer: Matcher, event: GroupMessageEvent):
     if group_json[str(event.group_id)] == False:
         pass
     if group_json[str(event.group_id)] == True:
-        bvnum = re.search('BV|bv|bV|Bv', str(event.get_message())).span()
-        start_num = int(bvnum[0])
-        bvid = str(event.get_message())[start_num: start_num + 12]
-        info = Video(bvid)
+        Original_Msg = str(event.get_message())
+        if 'http://b23.tv/' or 'https://b23.tv/' in Original_Msg:
+            try:
+                urlnum = re.search('https://b23.tv/', Original_Msg, re.IGNORECASE).span()
+            except AttributeError:
+                urlnum = re.search('http://b23.tv/', Original_Msg, re.IGNORECASE).span()
+            url = Original_Msg[int(urlnum[0]) : int(urlnum[1] + 7)]
+            resp = requests.get(url)
+            bvid = get_bvid(str(resp.url))
+        else:
+            bvid = get_bvid(Original_Msg)
+        info = await Video(bvid)
         data = info.data
         reply = MessageSegment.reply(event.message_id)
         if info.status == True:
@@ -59,9 +69,15 @@ async def analysis(bot: Bot, mathcer: Matcher, event: GroupMessageEvent):
             desc = data.get('desc')
             author = data.get('owner').get('name')
             msg = f'视频号：{bvid}  av{avid}\n标题：{title}\n简介：\n{desc}\n\n类型：{tname}\n作者：{author}\n视频链接：\nhttps://bilibili.com/video/{bvid}\nhttps://bilibili.com/video/av{avid}'
-            await auto_analysis.send(reply + pic + msg)
+            await auto_analysis.finish(reply + pic + msg)
         if info.status == False:
             pass
+
+def get_bvid(msg):
+        bvnum = re.search('BV', msg, re.IGNORECASE).span()
+        start_num = int(bvnum[0])
+        bvid = msg[start_num: start_num + 12]
+        return bvid
 
 function = on_command('bilitool',permission=GROUP,priority=49)
 @function.handle()
@@ -71,11 +87,11 @@ async def function(bot: Bot, mathcer: Matcher, event: GroupMessageEvent ,args: M
         group_json = Config_Read().group
         group_json[str(event.group_id)] = False
         Config_Write(group_json)
-        await auto_analysis.send('此群的BV号自动解析已关闭')
+        await auto_analysis.finish('此群的BV号自动解析已关闭')
     if arg == 'on':
         group_json = Config_Read().group
         group_json[str(event.group_id)] = True
         Config_Write(group_json)
-        await auto_analysis.send('此群的BV号自动解析已开启')
+        await auto_analysis.finish('此群的BV号自动解析已开启')
     if arg == 'help' or arg == '':
-        await auto_analysis.send(helpmessage)
+        await auto_analysis.finish(helpmessage)
